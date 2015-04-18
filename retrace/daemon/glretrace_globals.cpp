ac65679346805649d9074a123bf00807bf98ca97
Mark Janes <mark.a.janes@intel.com>
2015-04-17 17:03:00 -0700

    add Frame Retrace stub
    This initial implementation loads a file and plays to the frame
    point.  There are also a couple of unit tests, which may have to be
    removed if gtest is not in the cards for apitrace.

---
+++ retrace/daemon/glretrace_globals.cpp
@@ -0,0 +1,51 @@
+#include "trace_parser.hpp"
+#include "trace_profiler.hpp"
+#include "retrace.hpp"
+#include "trace_callset.hpp"
+
+static trace::CallSet snapshotFrequency;
+static trace::ParseBookmark lastFrameStart;
+
+static unsigned dumpStateCallNo = ~0;
+
+retrace::Retracer retracer;
+
+namespace retrace {
+
+trace::Parser parser;
+trace::Profiler profiler;
+
+
+int verbosity = 0;
+unsigned debug = 1;
+bool dumpingState = false;
+bool dumpingSnapshots = false;
+
+Driver driver = DRIVER_DEFAULT;
+const char *driverModule = NULL;
+
+bool doubleBuffer = true;
+unsigned samples = 1;
+
+bool profiling = false;
+bool profilingGpuTimes = false;
+bool profilingCpuTimes = false;
+bool profilingPixelsDrawn = false;
+bool profilingMemoryUsage = false;
+bool useCallNos = true;
+bool singleThread = false;
+
+unsigned frameNo = 0;
+unsigned callNo = 0;
+
+void
+frameComplete(trace::Call &call) {
+}
+
+static Dumper defaultDumper;
+
+Dumper *dumper = &defaultDumper;
+
+
+}
+
