c797d5269b7361792badeecebdd02e16ad9a0ed4
Mark Janes <mark.a.janes@intel.com>
2015-08-14 16:36:14 -0700

    Make lint tool callable from multiple directories.
    This commit moves the cpplint macros to an include file, and unifies
    the implementation so it can be invoked with a single macro call.

---
+++ retrace/daemon/Lint.cmake
@@ -0,0 +1,12 @@
+macro (Lint _FILE_LIST)
+  # build a list of full paths to files in the list
+  set (out_list "")
+  foreach( _in_file ${${_FILE_LIST}})
+    list (APPEND out_list ${CMAKE_CURRENT_SOURCE_DIR}/${_in_file})
+  endforeach(_in_file)
+  # make a unique target name for the current directory, by replacing
+  # illegal characters in the path
+  string(REPLACE "/" "_" lint_target ${CMAKE_CURRENT_SOURCE_DIR}lint)
+  # create a target to run lint on source files
+  add_custom_target(${lint_target} ALL python ${PROJECT_SOURCE_DIR}/retrace/daemon/cpplint.py --output=emacs ${out_list})
+endmacro(Lint)
