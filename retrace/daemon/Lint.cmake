e34a5b0d0c8448c69f3d7a2ff836949ce1f04927
Mark Janes <mark.a.janes@intel.com>
2015-11-30 13:45:21 -0800

    replace ':' in windows path, to make valid cmake target
    Lint.cmake's macros did not take into account Windows quirks when
    defining macros.

--- retrace/daemon/Lint.cmake
+++ retrace/daemon/Lint.cmake
@@ -6,7 +6,8 @@ macro (Lint _FILE_LIST)
   endforeach(_in_file)
   # make a unique target name for the current directory, by replacing
   # illegal characters in the path
-  string(REPLACE "/" "_" lint_target ${CMAKE_CURRENT_SOURCE_DIR}lint)
+  string(REPLACE "/" "_" lint_target1 ${CMAKE_CURRENT_SOURCE_DIR}lint)
+  string(REPLACE ":" "_" lint_target ${lint_target1})
   # create a target to run lint on source files
   add_custom_target(${lint_target} ALL python ${PROJECT_SOURCE_DIR}/retrace/daemon/cpplint.py --output=emacs ${out_list})
 endmacro(Lint)
