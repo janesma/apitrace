cc50577e79ea211dc9d33c8bc7837519e75c2c04
Mark Janes <mark.a.janes@intel.com>
2017-01-06 16:06:58 -0800

    Shaders: retrace compiled shaders only on matching selected renders
    Previously, compiling a shader via the UI simply overrided the program
    for the last selected render.  The correct action is to override the
    selected program for all selected renders where it used.

--- retrace/daemon/ui/glframe_shader_model.cpp
+++ retrace/daemon/ui/glframe_shader_model.cpp
@@ -28,10 +28,19 @@
 #include "glframe_shader_model.hpp"
 
 #include <sstream>
+#include <string>
 #include <vector>
 
+#include "glframe_logger.hpp"
+#include "glframe_os.hpp"
+#include "glframe_retrace_model.hpp"
+
+using glretrace::DEBUG;
+using glretrace::FrameRetraceModel;
 using glretrace::QRenderShaders;
 using glretrace::QRenderShadersList;
+using glretrace::ScopedLock;
+
 void
 QRenderShaders::onShaderAssembly(const ShaderAssembly &vertex,
                                  const ShaderAssembly &fragment,
@@ -56,7 +65,9 @@ QRenderShadersList::onShaderAssembly(RenderId renderId,
                                      const ShaderAssembly &tess_eval,
                                      const ShaderAssembly &geom,
                                      const ShaderAssembly &comp) {
+  ScopedLock s(m_protect);
   if (selectionCount != m_current_selection) {
+    m_renders.clear();
     m_shader_assemblies.clear();
     m_render_strings.clear();
     m_current_selection = selectionCount;
@@ -69,6 +80,7 @@ QRenderShadersList::onShaderAssembly(RenderId renderId,
         (tess_eval.shader == m_shader_assemblies[i][3].shader) &&
         (geom.shader == m_shader_assemblies[i][4].shader) &&
         (comp.shader == m_shader_assemblies[i][5].shader)) {
+      m_renders[i].push_back(renderId);
       m_render_strings[i].append(QString(",%1").arg(renderId.index()));
       emit onRendersChanged();
       return;
@@ -76,6 +88,8 @@ QRenderShadersList::onShaderAssembly(RenderId renderId,
   }
 
   // program not found
+  m_renders.push_back(std::vector<RenderId>());
+  m_renders.back().push_back(renderId);
   m_render_strings.push_back(QString("%1").arg(renderId.index()));
   m_shader_assemblies.push_back(std::vector<ShaderAssembly>());
   m_shader_assemblies.back().push_back(ShaderAssembly());
@@ -93,18 +107,19 @@ QRenderShadersList::onShaderAssembly(RenderId renderId,
 
   if (m_render_strings.size() == 1)
     // first shader assembly for a new selection.  update shader display
-    setIndex(0);
+    setIndexDirect(0);
 
   emit onRendersChanged();
 }
 
 QStringList
 QRenderShadersList::renders() {
+  ScopedLock s(m_protect);
   return m_render_strings;
 }
 
 void
-QRenderShadersList::setIndex(int index) {
+QRenderShadersList::setIndexDirect(int index) {
   m_shaders.onShaderAssembly(m_shader_assemblies[index][0],
                              m_shader_assemblies[index][1],
                              m_shader_assemblies[index][2],
@@ -112,3 +127,48 @@ QRenderShadersList::setIndex(int index) {
                              m_shader_assemblies[index][4],
                              m_shader_assemblies[index][5]);
 }
+
+void
+QRenderShadersList::setIndex(int index) {
+  ScopedLock s(m_protect);
+  setIndexDirect(index);
+}
+
+void
+QRenderShadersList::setRetrace(IFrameRetrace *retracer,
+                               FrameRetraceModel *model) {
+  m_retracer = retracer;
+  m_retraceModel = model;
+}
+
+void
+QRenderShadersList::overrideShaders(int index,
+                                    const QString &vs, const QString &fs,
+                                    const QString &tess_control,
+                                    const QString &tess_eval,
+                                    const QString &geom,
+                                    const QString &comp) {
+  ScopedLock s(m_protect);
+  const std::string &vss = vs.toStdString(),
+                    &fss = fs.toStdString(),
+         &tess_control_s = tess_control.toStdString(),
+            &tess_eval_s = tess_eval.toStdString(),
+                 &geom_s = geom.toStdString(),
+                 &comp_s = comp.toStdString();
+
+  GRLOGF(DEBUG, "vs: %s\nfs: %s\ntess_control: %s\n"
+         "tess_eval: %s\ngeom: %s\ncomp: %s",
+         vss.c_str(), fss.c_str(),
+         tess_control_s.c_str(), tess_eval_s.c_str(), geom_s.c_str(),
+         comp_s.c_str());
+  for (auto i : m_renders[index]) {
+    m_retracer->replaceShaders(i, ExperimentId(0), vss, fss,
+                               tess_control_s, tess_eval_s, geom_s, comp_s,
+                               m_retraceModel);
+  }
+  m_renders.clear();
+  m_shader_assemblies.clear();
+  m_render_strings.clear();
+  m_retraceModel->onShadersChanged();
+}
+
