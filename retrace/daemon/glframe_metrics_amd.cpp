2f3eae6dd0fe3614be6cadd8c1e4211763e9dade
Mark Janes <mark.a.janes@intel.com>
2019-05-01 11:52:28 -0700

    Metrics: Allow duplicate AMD metrics
    The Iris driver reports the same metric in several groups.

--- retrace/daemon/glframe_metrics_amd.cpp
+++ retrace/daemon/glframe_metrics_amd.cpp
@@ -182,7 +182,6 @@ PerfMetricsContextAMD::PerfMetricsContextAMD(OnFrameRetrace *cb)
       metrics.clear();
       g->metrics(&metrics);
       for (auto &d : metrics) {
-        assert(known_metrics.find(d.name) == known_metrics.end());
         known_metrics[d.name] = d;
         metric_map[d.id] = m_groups.size() - 1;
       }
